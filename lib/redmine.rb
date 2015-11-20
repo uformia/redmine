cf93cedab42de7d1f06a57c8f292e118b9be0536
Peter Newman <peter@uformia.com>
2015-11-20 05:11:46 +0000

    Removed Home (welcome) page, goes to projects controller by default

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -184,9 +184,8 @@ Redmine::AccessControl.map do |map|
 end
 
 Redmine::MenuManager.map :top_menu do |menu|
-  menu.push :home, :home_path
-  menu.push :my_page, { :controller => 'my', :action => 'page' }, :if => Proc.new { User.current.logged? }
   menu.push :projects, { :controller => 'projects', :action => 'index' }, :caption => :label_project_plural
+  menu.push :my_page, { :controller => 'my', :action => 'page' }, :if => Proc.new { User.current.logged? }
   menu.push :administration, { :controller => 'admin', :action => 'index' }, :if => Proc.new { User.current.admin? }, :last => true
   menu.push :help, Redmine::Info.help_url, :last => true
 end
